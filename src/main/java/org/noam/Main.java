2219df8c4d3278b4aec50b2e1d6f6d8a5427225b
Noam Manyfler <noam.manyfler@otorio.com>
2020-07-23 11:33:03 +0300

    Partition by number of CPUs

--- src/main/java/org/noam/Main.java
+++ src/main/java/org/noam/Main.java
@@ -1,5 +1,6 @@
 package org.noam;
 
+import com.google.common.collect.Lists;
 import org.noam.model.OutputModel;
 import org.noam.output.OutputToFile;
 import org.noam.transform.TransformStream;
@@ -7,6 +8,7 @@ import org.noam.transform.TransformStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,13 +18,16 @@ public class Main {
         final Logger logger = Logger.getLogger(Main.class.getName());
         final String INPUT_FILE_PATH = Paths.get("").toAbsolutePath().toString() + "/raw/";
         final String INPUT_FILE_NAME = "spins_input.json";
+        final int CORES = Runtime.getRuntime().availableProcessors();
+
         final OutputToFile outputToFile = new OutputToFile();
 
         try (Stream<String> stream = Files.lines(Paths.get(INPUT_FILE_PATH + INPUT_FILE_NAME))) {
             Stream<OutputModel> outputModelsStream = new TransformStream(stream).transform();
-            outputModelsStream
-                    .collect(Collectors.groupingByConcurrent(OutputModel::getSpins))
-                    .entrySet()
+
+            // Partition the stream to a fixed number of sub lists (number of cores)
+            List<OutputModel> numberOfOutputStreams = outputModelsStream.collect(Collectors.toUnmodifiableList());
+            Lists.partition(numberOfOutputStreams, (numberOfOutputStreams.size()) / CORES + 1)
                     .parallelStream().forEach(outputToFile::saveToFile);
         } catch (IOException e) {
             logger.severe("Input file not found on path: " + INPUT_FILE_PATH + INPUT_FILE_NAME);
--- src/main/java/org/noam/output/OutputToFile.java
+++ src/main/java/org/noam/output/OutputToFile.java
@@ -1,17 +1,20 @@
 package org.noam.output;
 
+import org.noam.Main;
 import org.noam.model.OutputModel;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
 
 public class OutputToFile {
 
     private String fileName = "_part.csv";
     private String path = "output/";
+    private AtomicInteger atomicInteger = new AtomicInteger();
 
     public OutputToFile() {}
 
@@ -28,12 +31,16 @@ public class OutputToFile {
         return path;
     }
 
-    public void saveToFile(Map.Entry<Integer, List<OutputModel>> entry) {
-        final File csvOutputFile = new File(this.path + entry.getKey() + this.fileName);
+    public AtomicInteger getAtomicInteger() {
+        return atomicInteger;
+    }
+
+    public void saveToFile(List<OutputModel> outputModels) {
+        final File csvOutputFile = new File(this.path + this.getAtomicInteger().getAndIncrement() + this.fileName);
         try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-            entry.getValue().forEach(pw::println);
+            outputModels.forEach(pw::println);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            Logger.getLogger(Main.class.getName()).severe("Could not write output file, directory does not exists");
         }
     }
 }
